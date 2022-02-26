6bd95865f8da772740aaf08763f1cbd9662f1689
Traitet Thepbandansuk <traitet@hotmail.com>
2022-02-27 00:00:30 +0700

    sql

--- database/sqlqueries/2.interface_sap_rec.sql
+++ database/sqlqueries/2.interface_sap_rec.sql
@@ -1,6 +1,6 @@
 USE [SIAM_EPSINFDB]
 GO
-/****** Object:  StoredProcedure [dbo].[interface_sap_po]    Script Date: 23/02/2022 3:34:58 PM ******/
+/****** Object:  StoredProcedure [dbo].[interface_sap_rec]    Script Date: 23/02/2022 3:34:58 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -24,17 +24,17 @@ BEGIN
     -- DECLARE @doc_num AS VARCHAR(100) = ''
     -- DECLARE @record_count AS INT = 100
     SELECT top (@record_count )
-    RIDTTIFRC, RCDATE, SAPDOCNO, PONUM, ITEMID, INVNUM, POSTINGDATE, QUANTITY, STORELOC, LINEID, 	
-    SECTIONID, IFSTATUS, IFMESSEGE, SAPMATDOCNO, SAPMATDOCYEAR, SAPITEMID, OPTFIELD1, 	
-    OPTFIELD2, OPTFIELD3, OPTFIELD4, OPTFIELD5, OPTFIELD6, OPTFIELD7 
+    RIDTTIFRC, RCDATE, SAPDOCNO, PONUM, ITEMID, INVNUM, POSTINGDATE, QUANTITY, STORELOC, LINEID,
+    SECTIONID, IFSTATUS, IFMESSEGE, SAPMATDOCNO, SAPMATDOCYEAR, SAPITEMID, OPTFIELD1,
+    OPTFIELD2, OPTFIELD3, OPTFIELD4, OPTFIELD5, OPTFIELD6, OPTFIELD7
 
-    FROM TTIFREC 
-    WHERE 
-        IFSTATUS = 'E' and 
-        POSTINGDATE >= @start_date and 
-        POSTINGDATE <= @end_date and 
-        PONUM LIKE '%' + @doc_num + '%' and 
-        INVNUM LIKE  '%' + @inv_num + '%'  
+    FROM TTIFREC
+    WHERE
+        IFSTATUS = 'E' and
+        POSTINGDATE >= @start_date and
+        POSTINGDATE <= @end_date and
+        PONUM LIKE '%' + @doc_num + '%' and
+        INVNUM LIKE  '%' + @inv_num + '%'
         ORDER BY RIDTTIFRC, PONUM
 END
 
--- database/sqlqueries/4.emfg_shipping_log_ok.sql
+++ database/sqlqueries/4.emfg_shipping_log_ok.sql
@@ -1,6 +1,5 @@
 USE [SIAM_SHIPPINGDB];
 GO
-/****** Object:  StoredProcedure [dbo].[interface_sap_inv]    Script Date: 23/02/2022 3:39:54 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -19,12 +18,12 @@ ALTER PROCEDURE [dbo].[emfg_shipping_log_ok]
 AS
 
 SELECT TOP (@record_count )
-	DATE_CHK, TIME_CHK, PDS_NUM, PART_NUM, PART_NAME, PART_SA, PACK_QTY, PACKED, USER_CHK 
-FROM 
-	TM_LOG_OK 
-WHERE 
-	PDS_NUM = @doc_num AND 
-	DATE_CHK >= @start_date AND 
+	DATE_CHK, TIME_CHK, PDS_NUM, PART_NUM, PART_NAME, PART_SA, PACK_QTY, PACKED, USER_CHK
+FROM
+	TM_LOG_OK
+WHERE
+	PDS_NUM = @doc_num AND
+	DATE_CHK >= @start_date AND
 	DATE_CHK <= @end_date
 
 -- EXEC emfg_shipping_log_ok '20190101', '20220101','',100
