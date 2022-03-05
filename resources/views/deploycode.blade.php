ee72afe4697007716358d1ff96991110efc0fbe6
Traitet Thepbandansuk <traitet@hotmail.com>
2022-03-06 00:35:25 +0700

    deploy

--- resources/views/deploycode.blade.php
+++ resources/views/deploycode.blade.php
@@ -82,7 +82,7 @@
                     <div class="container-fluid">
                         <div class="d-sm-flex align-items-center justify-content-between mb-4">
 
-                            <h1 class="h3 mb-0 text-gray-800">Git pull - Deploy wissdemo01 from GitHub branch main </h1>
+                            <h1 class="h3 mb-0 text-gray-800">Git pull - Deploy wissdemo01 from GitHub main branch  </h1>
                         </div>
                         <div class="row">
                             <div class="col-xl-12 col-lg-12">
